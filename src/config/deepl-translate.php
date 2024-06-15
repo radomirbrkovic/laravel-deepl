86403fe83709ddc37102a15afd5d8e08636d4fc7
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-06-15 12:06:13 +0200

    Add options in config

--- src/config/deepl-translate.php
+++ src/config/deepl-translate.php
@@ -2,5 +2,10 @@
 
 return [
   'auth_key' => env('DEEPL_AUTH_KEY', ''),
-  'default_lang' => env('APP_DEFAULT_LANG', 'en'),
+  'default_lang' => env('APP_LANG', 'en'),
+  'options' => [
+    'split_sentences' => 'on',
+    'preserve_formatting' => false,
+    'formality' => 'default'
+  ],
 ];
