4a90aaaf451698aca5b2fba30b4cdf3627f356a1
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-06-25 06:02:48 +0200

    Fix config issue

--- src/Providers/DeeplTranslateServiceProvider.php
+++ src/Providers/DeeplTranslateServiceProvider.php
@@ -7,8 +7,9 @@ use Illuminate\Support\ServiceProvider;
 
 class DeeplTranslateServiceProvider extends ServiceProvider
 {
-  public function boot() {
-      $this->registerConfig();
+  public function boot()
+  {
+    $this->registerConfig();
 
     $this->app->singleton('deepl-translate', function () {
 
@@ -24,10 +25,9 @@ class DeeplTranslateServiceProvider extends ServiceProvider
   private function registerConfig()
   {
     $this->publishes([
-      __DIR__ . '/../../config/deepl-translate.php' => config_path('deepl-translate.php'),
-    ], 'genera');
+      __DIR__ . '/../config/deepl-translate.php' => config_path('deepl-translate.php'),
+    ]);
 
-    $this->mergeConfigFrom(__DIR__ . '/../../config/deepl-translate.php', 'deepl-translate');
   }
 
 
